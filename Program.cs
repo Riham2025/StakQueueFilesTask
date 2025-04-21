d0d3384c07b3a013a948c10092a329a02a0d5f94
riham <riham91.alsiyabi@gmail.com>
2025-04-21 15:35:39 +0400

    //Reverse a String Using Stack

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace StakQueueFilesTask
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace StakQueueFilesTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Reverse a String Using Stack 
+
+
+
+
+
         }
     }
 }
