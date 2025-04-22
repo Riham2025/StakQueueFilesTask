a84af174477abfd9eb41cc4c3213b6b8e070567c
riham <riham91.alsiyabi@gmail.com>
2025-04-22 09:31:59 +0400

    // complate funcation one

--- Program.cs
+++ Program.cs
@@ -6,16 +6,85 @@ namespace StakQueueFilesTask
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        public static void StartSystem()
+        
+            
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the String Reverser System!");
+            Console.WriteLine("choice the number");
+            Console.WriteLine("1. Enter a string to reverse:");
+            Console.WriteLine("2. Evaluate Postfix Expression");
 
-            //Reverse a String Using Stack 
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter a string to reverse:");
+                    string input = Console.ReadLine();
+                    string reversedString = ReverseString(input);
+                    Console.ReadLine();
+
+                    Console.WriteLine($"Reversed string: {reversedString}");
+                    break;
+               
+                case 2:
+                    Console.WriteLine("Enter a postfix expression:");
+                    string postfixExpression = Console.ReadLine();
+                    double result = EvaluatePostfix(postfixExpression);
+                    Console.WriteLine($"Result: {result}");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+
+        //Reverse a String Using Stack 
+
+        public static string ReverseString(string str)
+        {
+            string reversedString="";
+            Stack<char>  stack = new Stack<char>();
+
+            foreach (char ch in str)
+            {
+                stack.Push(ch);
+            }
+
+            foreach (char ch2 in stack)
+            {
+                Console.Write(ch2);
+
+            }
 
 
-
-
+            int choice = int.Parse(Console.ReadLine());
+           
+            return reversedString;// complate funcation one
 
         }
+        //Evaluate Postfix Expression Using Stack
+
+
+
+
+        static void Main(string[] args)
+        {
+            StartSystem();
+           
+
+            
+        }
+
+
+
+
+
+
+
+
+
     }
 }
